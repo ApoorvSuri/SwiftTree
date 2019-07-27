f02c29bb89e0031744bf925b49c25f4e588982e9
Apoorv <apoorva@cronycle.com>
2019-07-27 17:19:29 +0530

    Initial Commit

---
+++ SwiftTree/SwiftTree.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTree.h
+//  SwiftTree
+//
+//  Created by cronycle on 27/07/2019.
+//  Copyright © 2019 Apoorv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftTree.
+FOUNDATION_EXPORT double SwiftTreeVersionNumber;
+
+//! Project version string for SwiftTree.
+FOUNDATION_EXPORT const unsigned char SwiftTreeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTree/PublicHeader.h>
+
+
